6c6898c1c977ecd16165ce1cbcd35ba37fc47c47
Majdi Zerelli <zerellimajdi@gmail.com>
2024-01-14 09:33:02 +0100

    Setup Laravel Sanctum for API

--- routes/tenant.php
+++ routes/tenant.php
@@ -1,12 +1,9 @@
 <?php
-
 declare(strict_types=1);
-
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
-
 /*
 |--------------------------------------------------------------------------
 | Tenant Routes
@@ -18,26 +15,40 @@ use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 | Feel free to customize them however you want. Good luck!
 |
 */
-
 Route::middleware([
     'web',
     InitializeTenancyByDomain::class,
     PreventAccessFromCentralDomains::class,
 ])->group(function () {
-
-
     Route::get('/', function () {
         return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
     });
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->middleware(['auth', 'verified'])->name('dashboard');
-
     Route::middleware('auth')->group(function () {
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
         Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
-        require __DIR__ . '/auth.php';
-
+    require __DIR__ . '/auth.php';
 });
+Route::middleware([
+    'api',
+    InitializeTenancyByDomain::class,
+    PreventAccessFromCentralDomains::class,
+])->prefix('/api/v1')
+    ->group(function () {
+        // todo add admin middleware
+        Route::prefix('/admin')->group((
+            function () {
+                require __DIR__ . '/tenant/api/v1/admin/api.php';
+            }
+        ));
+        // todo add member middleware
+        Route::prefix('/member')->group((
+            function () {
+                require __DIR__ . '/tenant/api/v1/member/api.php';
+            }
+        ));
+    });
